8f9e1c7956539f805637d45ab16bf8c85211b7ff
Brad Mitchell <bradley.r.mitchell@gmail.com>
2016-06-22 10:55:57 -0500

    Update bc-community-new-tab.user.js

--- bc-community-new-tab.user.js
+++ bc-community-new-tab.user.js
@@ -12,113 +12,135 @@
 // @downloadURL https://github.com/Bairdley/bc-community-right-click/raw/master/bc-community-new-tab.user.js
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
 // ==/UserScript==
+
 (function () {
-  var OS,
-  urlExt = '/s/question/',
-  dataID,
-  newTabCount = 0,
-  keyCodes,
-  keyed = false,
-  currentKey;
-  (function () {
-    if (navigator.appVersion.indexOf('Win') != - 1) {
-      OS = 'win';
-      keyCodes = {
-        ctrl: 17
-      };
-    } else {
-      OS = 'mac';
-      keyCodes = {
-        ctrl: 17,
-        cmdL: 91,
-        cmdR: 93,
-        cmdFF: 224
-      };
-    }
-  }());
-  $(document).keydown(function (e) {
-    for (var i in keyCodes) {
-      if (e.which === keyCodes[i]) {
-        currentKey = keyCodes[i];
-        keyed = true;
-      }
-    }
-  });
-  $(document).keyup(function (e) {
-    if (e.which === currentKey) {
-      keyed = false;
-    }
-  });
-  $('.compactFeedElement').live('mouseover', function () {
-    $(this).mousedown(function (e) {
-      var el = $(this).find('a').andSelf();
-      if ($(this).attr('data-id')) {
-        dataID = $(this).attr('data-id');
-        if (e.which !== 1 && keyed === false) {
-          clicks.right(el);
-        } else if (OS === 'win') {
-          if (keyed === true && currentKey === keyCodes.ctrl) {
-            clicks.win.ctrlClick(el);
-          }
-        } else if (OS === 'mac') {
-          var cmd;
-          switch (true) {
-            case currentKey === keyCodes.cmdFF:
-              clicks.mac.cmdClick(el);
-              break;
-            case currentKey === keyCodes.cmdL:
-              clicks.mac.cmdClick(el);
-              break;
-            case currentKey === keyCodes.cmdR:
-              clicks.mac.cmdClick(el);
-              break;
-            case currentKey === keyCodes.ctrl:
-              setHref.url(el);
-              break;
-          }
+  
+    var OS,
+        urlExt = '/s/question/',
+        dataID,
+        newTabCount = 0,
+        keyCodes,
+        keyed = false,
+        currentKey;
+  
+    (function () {
+        if (navigator.appVersion.indexOf('Win') != -1) {
+            OS = 'win';
+            keyCodes = {
+                ctrl: 17
+            };
+        } else {
+            OS = 'mac';
+            keyCodes = {
+                ctrl: 17,
+                cmdL: 91,
+                cmdR: 93,
+                cmdFF: 224
+            };
         }
-      }
-    }).mouseleave(function () {
-      setHref.void ($(this).find('a').andSelf());
-    });
-  });
-  function newTab() {
-    window.open(urlExt + dataID);
-    var int = setInterval(function () {
-      if (newTabCount > 0) {
-        newTabCount = 0;
-        window.clearInterval(int);
-      }
-    }, 500);
-  }
-  var setHref = {
-    url: function (el) {
-      $(el).attr('href', urlExt + dataID);
-    },
-    void : function (el) {
-      $(el).attr('href', 'javascript:void(0)');
-      keyed = false;
+    }());
+  
+    $(document)
+        .keydown(function (e) {
+            for (var i in keyCodes) {
+                if (e.which === keyCodes[i]) {
+                    currentKey = keyCodes[i];
+                    keyed = true;
+                }
+            }
+        });
+  
+    $(document)
+        .keyup(function (e) {
+            if (e.which === currentKey) {
+                keyed = false;
+            }
+        });
+  
+    $('.compactFeedElement')
+        .live('mouseover', function () {
+            $(this)
+                .mousedown(function (e) {
+                    var el = $(this)
+                        .find('a')
+                        .andSelf();
+                    if ($(this)
+                        .attr('data-id')) {
+                        dataID = $(this)
+                            .attr('data-id');
+                        if (e.which !== 1 && keyed === false) {
+                            clicks.right(el);
+                        } else if (OS === 'win') {
+                            if (keyed === true && currentKey === keyCodes.ctrl) {
+                                clicks.win.ctrlClick(el);
+                            }
+                        } else if (OS === 'mac') {
+                            var cmd;
+                            switch (true) {
+                            case currentKey === keyCodes.cmdFF:
+                                clicks.mac.cmdClick(el);
+                                break;
+                            case currentKey === keyCodes.cmdL:
+                                clicks.mac.cmdClick(el);
+                                break;
+                            case currentKey === keyCodes.cmdR:
+                                clicks.mac.cmdClick(el);
+                                break;
+                            case currentKey === keyCodes.ctrl:
+                                setHref.url(el);
+                                break;
+                            }
+                        }
+                    }
+                })
+                .mouseleave(function () {
+                    setHref.void($(this)
+                        .find('a')
+                        .andSelf());
+                });
+        });
+
+    function newTab() {
+        window.open(urlExt + dataID);
+        var int = setInterval(function () {
+            if (newTabCount > 0) {
+                newTabCount = 0;
+                window.clearInterval(int);
+            }
+        }, 500);
     }
-  };
-  var clicks = {
-    mac: {
-      cmdClick: function (el) {
-        while (newTabCount < 1) {
-          newTab();
-          newTabCount += 1;
+  
+    var setHref = {
+        url: function (el) {
+            $(el)
+                .attr('href', urlExt + dataID);
+        },
+        void: function (el) {
+            $(el)
+                .attr('href', 'javascript:void(0)');
+            keyed = false;
         }
-      }
-    },
-    win: {
-      ctrlClick: function (el) {
-        while (newTabCount < 1) {
-          newTab();
-          newTabCount += 1;
+    };
+  
+    var clicks = {
+        mac: {
+            cmdClick: function (el) {
+                while (newTabCount < 1) {
+                    newTab();
+                    newTabCount += 1;
+                }
+            }
+        },
+        win: {
+            ctrlClick: function (el) {
+                while (newTabCount < 1) {
+                    newTab();
+                    newTabCount += 1;
+                }
+            }
+        },
+        right: function (el) {
+            setHref.url(el);
         }
-      }
-    },
-    right: function (el) {
-      setHref.url(el);
-    }
-  };
-}) ();
+    };
+})();
