21b22e034dabea4a0820291fb5c986fbf81c97c3
Brad Mitchell <bradley.r.mitchell@gmail.com>
2016-08-29 19:31:06 -0500

    documentation

--- bc-community-new-tab.user.js
+++ bc-community-new-tab.user.js
@@ -16,7 +16,8 @@
 (function() {
     var staticLinks = [],
         url = 'https://spreadsheets.google.com/feeds/list/1Fk_RrRaB_-dn_L4pxhHY1usSXlUsq0xelDUpyMSh8IQ/1/public/basic?alt=json';
-
+    
+    //Build array of static links, which are managed via spreadsheet
     $.ajax({
         url: url,
         success: function(data) {
@@ -28,7 +29,8 @@
             });
         }
     });
-
+    
+    //define key event
     var keys = {
         pressed: false,
         codes: {
@@ -48,19 +50,22 @@
             keys.pressed = false;
         }
     };
-
+   
+   //capture key event
     $(document).keydown(function(e) {
         keys.down(e.which);
     }).keyup(function(e) {
         keys.up();
     });
     
+    //define mouse event
     var mouse = {
         button: function(e) {
             return e.which === 1 ? 'left' : 'right';
         }
     };
-
+    
+    //defines the link that user is currently interacting with
     var link = {
         current: {
             el: '',
@@ -70,9 +75,11 @@
         },
         static: {
             define: function(el) {
+                //if user is interacting with a list item, check text against the static links in the spreadsheet
                 var linkText = $(el).text();
                 for (var i = 0; i < staticLinks.length; i++) {
                     if (staticLinks[i][0] === linkText) {
+                        //if link is found in the spreadsheet, set current link
                         link.current = {
                             el: $(el).find('a'),
                             type: 'static',
@@ -85,15 +92,21 @@
         },
         dynamic: {
             define: function(el) {
+                //all links begin with /s/
                 var urlBegin = '/s/',
+                    //datasource refers to the id that is appended to the ends of dynamic URLs
                     dataSource;
+                //three types of dataSource: 
                 if ($(el).find('a').andSelf().hasClass('compactFeedElement')) {
+                    //feed links always include /question/<data-id> 
                     urlBegin += 'question/';
                     dataSource = 'data-id';
                 } else if ($(el).find('a').andSelf().hasClass('cuf-entityLinkId')) {
+                    //profile links always include /profile/<data-id> 
                     urlBegin += 'profile/';
                     dataSource = 'data-id';
                 } else if ($(el).find('a').andSelf().hasClass('forceChatterCompoundFieldsGroupName')) {
+                    //group links always include /group/<data-recordid>
                     urlBegin += 'group/';
                     dataSource = 'data-recordid';
                 }
@@ -107,19 +120,27 @@
             }
         },
         action: {
+            //on right click, link's href is set to the URL of the destination page
+            //this allows the user to interact with the link traditionally
             setHref: function() {
                 $(link.current.el).attr('href', link.current.url);
             },
+            //returns href attribute to 'javascript:void(0)'
             voidHref: function() {
                 $(link.current.el).attr('href', 'javascript:void(0)');
             },
+            //on ctrl/cmd-click, the SPA sometimes still loads the link in the original tab
+            //this loading is controlled by 'data-aura-rendered by' attribute
+            //removes this attribute momentarily
             removeDataAura: function() {
                 $(link.current.el).attr('data-aura-rendered-by', '');
             },
+            //restores data-aura-rendered-by attribute (for future left-clicks)
             restoreDataAura: function() {
                 $(link.current.el).attr('data-aura-rendered-by', link.current.dataAura);
             },
             newTab: function() {
+                //sometimes, new tab will gain focus, meaning keys.pressed will === true until pressed again
                 var windowFocus;
                 $(window).focus(function() {
                     windowFocus = true;
@@ -127,6 +148,7 @@
                     windowFocus = false;
                 });
                 window.open(link.current.url);
+                //if window loses focus, keys.pressed defaults back to false
                 if (windowFocus === false) {
                     keys.pressed = false;
                 }
@@ -138,21 +160,27 @@
         $(el).on({
             mousedown: function(e) {
                 if (mouse.button(e) === 'right') {
+                    //set href on mousedown so that it can be interpreted on mouseup
                     link.action.setHref();
                 } else if (mouse.button(e) === 'left' && keys.pressed === true) {
+                    //remove data-aura-rendered-by before mouseup to prevent SPA from loading link in original tab
                     link.action.removeDataAura();
                 }
             },
             mouseup: function(e) {
                 if (mouse.button(e) === 'left' && keys.pressed) {
+                    //if ctrl/cmd-click, open link in new tab
                     e.stopImmediatePropagation();
                     link.action.newTab();
+                    //wait 200ms after mouseup before returning data-aura-rendered-by attribute
+                    //this timeout prevents the SPA from loading the link in the original tab
                     setTimeout(function() {
                         link.action.restoreDataAura();
                     }, 200);
                 }
             },
             mouseleave: function() {
+                //fail-safe: just in case the user leaves the link before mouseup
                 if ($(link.current.el).attr('data-aura-rendered-by') === '') {
                     link.action.restoreDataAura();
                 }
@@ -160,8 +188,9 @@
             }
         });
     }
-
+    
     $(document).mouseover(function() {
+        //attaches listeners to list element when hovered
         $('li').on({
             mouseenter: function(e) {
                 e.stopImmediatePropagation();
@@ -169,6 +198,7 @@
                 addListeners($(this));
             }
         });
+        //attaches listeners to dynamic feed elements when hovered
         $('.compactFeedElement, .cuf-entityLinkId, .forceChatterCompoundFieldsGroupName').on({
             mouseenter: function(e) {
                 e.stopImmediatePropagation();
